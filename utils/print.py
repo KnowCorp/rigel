1b5441e84ca0fb44ec9cce1c75cac7cbd518a60c
Hassan <h.asqiriba@pm.me>
2022-12-20 14:11:16 -0800

    build: update logging system

--- utils/print.py
+++ utils/print.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from .color import Color
 
 import logging
@@ -17,41 +18,51 @@ def console_print(msg: str = None, status: str = None, **kwargs) -> None:
     """
 
     if msg is None or status is None:
+        date = datetime.now().strftime("%Y%m%d %X")
+
         for key, value in kwargs.items():
             value = str(value)
 
             if key == "error":
                 logging.error(
-                    emojis.encode(f":no_entry: {Color.RED}{value.title()}{Color.END}")
+                    emojis.encode(
+                        f":no_entry: {Color.RED}{value}{Color.END} [ {Color.YELLOW}{date}{Color.END} ]"
+                    )
                 )
             elif key == "warning":
                 logging.warning(
-                    emojis.encode(f":warning: {Color.YELLOW}{value.title()}{Color.END}")
+                    emojis.encode(
+                        f":warning: {Color.YELLOW}{value}{Color.END} [ {Color.YELLOW}{date}{Color.END} ]"
+                    )
                 )
             elif key == "info":
                 logging.info(
                     emojis.encode(
-                        f" :information_source: {Color.CYAN}{value.title()}{Color.END}"
+                        f" :information_source: {Color.CYAN}{value}{Color.END} [ {Color.YELLOW}{date}{Color.END} ]"
                     )
                 )
             elif key == "success":
                 logging.info(
-                    emojis.encode(f":ok: {Color.GREEN}{value.title()}{Color.END}")
+                    emojis.encode(
+                        f":ok: {Color.GREEN}{value}{Color.END} [ {Color.YELLOW}{date}{Color.END} ]"
+                    )
                 )
             elif key == "debug":
                 logging.info(
-                    emojis.encode(f":snake: {Color.PURPLE}{value.title()}{Color.END}")
+                    emojis.encode(
+                        f":snake: {Color.PURPLE}{value}{Color.END} [ {Color.YELLOW}{date}{Color.END} ]"
+                    )
                 )
             else:
                 logging.info(
                     emojis.encode(
-                        f":small_orange_diamond: {Color.BOLD}{value.title()}{Color.END}"
+                        f":heavy_minus_sign: {Color.BOLD}{value}{Color.END} [ {Color.YELLOW}{date}{Color.END} ]"
                     )
                 )
 
     else:
         logging.info(
             emojis.encode(
-                f":heavy_minus_sign: {Color.PURPLE}{msg.title()} [{Color.GREEN} {status} {Color.PURPLE}]{Color.END}"
+                f":snake: {Color.PURPLE}{msg} [ {Color.YELLOW} {status} {Color.PURPLE} ]{Color.END}"
             )
         )
